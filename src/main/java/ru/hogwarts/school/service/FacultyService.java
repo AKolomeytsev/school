729124a84c16ee1a05621bcfa34dd93d6f7e8f03
AKolomeytsev <a.kolomeytsev@list.ru>
2022-03-23 09:05:06 +0300

    4.4 Логирование и конфигурирование в приложении.

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.repository.RepositoryFaculty;
@@ -10,31 +12,39 @@ import java.util.Collection;
 public class FacultyService {
     private final RepositoryFaculty repositoryFaculty;
 
+    private Logger logger = LoggerFactory.getLogger(FacultyService.class);
+
     public FacultyService(RepositoryFaculty faculty) {
         this.repositoryFaculty = faculty;
     }
 
     public Faculty createFaculty(Faculty faculty){
+        logger.info("Был вызван метод создания факультета");
         return repositoryFaculty.save(faculty);
     }
 
     public Faculty getFaculty(Long id){
+        logger.info("Был вызван метод поиска факультета по ID");
         return repositoryFaculty.findById(id).get();
     }
 
     public Faculty updateFaculty(Faculty faculty){
+        logger.info("Был вызван метод редактирования данных факультета");
         return repositoryFaculty.save(faculty);
     }
 
     public void deleteFaculty(Long id){
+        logger.info("Был вызван метод удаления факультета по ID");
         repositoryFaculty.deleteById(id);
     }
 
     public Collection<Faculty> findByColorIgnoreCaseContains(String color){
+        logger.info("Был вызван метод получения списка факультетов по цвету");
         return (Collection<Faculty>) repositoryFaculty.findByColorIgnoreCaseContains(color);
     }
 
     public Collection<Faculty> findByNameIgnoreCaseContains(String name){
+        logger.info("Был вызван метод поиска факультета по названию");
         return (Collection<Faculty>) repositoryFaculty.findByNameIgnoreCaseContains(name);
     }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Student;
@@ -11,43 +13,54 @@ import java.util.Collection;
 public class StudentService {
     private final RepositoryStudent repositoryStudent;
 
+    private Logger logger = LoggerFactory.getLogger(StudentService.class);
+
     public StudentService(RepositoryStudent student) {
         this.repositoryStudent = student;
     }
 
     public Student createStudent(Student student){
+        logger.info("Был вызван метод создания студента");
         return repositoryStudent.save(student);
     }
 
     public Student getStudent(Long id){
+        logger.info("Был вызван метод поиска студента по ID");
         return repositoryStudent.findById(id).get();
     }
 
     public Student updateStudent(Student student){
+        logger.info("Был вызван метод редактирования информации о студенте");
         return repositoryStudent.save(student);
     }
 
     public void deleteStudent(Long id){
+        logger.info("Был вызван метод удаления информации о студенте по ID");
         repositoryStudent.deleteById(id);
     }
 
     public Collection<Student> getStudentsAge(int age){
+        logger.info("Был вызван метод получения списка студентов по возрасту");
         return repositoryStudent.findByAge(age);
     }
 
     public Collection<Student> findByAgeBetween(int minAge, int maxAge){
+        logger.info("Был вызван метод получения списка студентов по возрасту в промежутке");
         return repositoryStudent.findByAgeBetween(minAge,maxAge);
     }
 
     public int getCountStudent(){
+        logger.info("Был вызван метод получения количества студентов");
         return repositoryStudent.getCountStudent();
     }
 
     public int getAvgAgeStudent(){
+        logger.info("Был вызван метод получения среднего возраста студентов");
         return repositoryStudent.getAvgAgeStudent();
     }
 
     public Collection<Student> getFiveLastStudent(){
+        logger.info("Был вызван метод получения списка из 5 студетов");
         return repositoryStudent.getFiveLastStudent();
     }
 }
